f260a55eeb50c2e6429cfcbe84727e86901ba064
Nikhil Badhe <nikhil.badhe@atidan.com>
2026-03-08 01:45:34 +0530

    Added the AuthentiactionAPI Project

---
+++ ECommerce.AuthenticationAPISolution/AuthenticationAPI.Infrastructure/Data/AuthenticationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using AuthenticationAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationAPI.Infrastructure.Data
+{
+    public class AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : DbContext(options)
+    {
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ ECommerce.AuthenticationAPISolution/AuthenticationAPI.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,43 @@
+﻿using AuthenticationAPI.Application.DTOs;
+using AuthenticationAPI.Application.Interfaces;
+using Azure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationAPI.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController(IUser userInterface) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<Response>> Register(AppUserDTO appUserDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var response = await userInterface.Register(appUserDTO);
+            return response.Flag ? Ok(response) : BadRequest(response);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<Response>> Login(LoginDTO loginDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var response = await userInterface.Login(loginDTO);
+            return response.Flag ? Ok(response) : BadRequest(response);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<GetUserDTO>> GetUser(int id)
+        {
+            if (id <= 0)
+                return BadRequest("Invalid user ID.");
+            
+            var user = await userInterface.GetUser(id);
+            return user.Id > 0 ? Ok(user) : NotFound();
+        }
+    }
+}
